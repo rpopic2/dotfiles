c7412f366675c7e56b00852495f432d4455999ff
rpopic2 <rpopic2@gmail.com>
2022-11-18 08:29:34 +0000

    fix script bug

--- scripts/git-changes.sh
+++ scripts/git-changes.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-STATUS=$(git status)
 RESULT=""
-if [ -z $($STAT | grep 'tree clean')]; then
-    RESULT="*"
-fi
 if git status | grep -c push; then
     RESULT="$RESULT↑"
 fi
 if git status | grep -c pull; then
     RESULT="$RESULT↓"
 fi
-echo $RESULT
+if git status | grep -c 'tree clean'; then
+    echo $RESULT
+    exit
+fi
+echo "$RESULT*"
