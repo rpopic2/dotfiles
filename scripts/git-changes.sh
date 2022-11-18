c3b8e612b1cda12e2ed7be3cfd79c08e68568d70
rpopic2 <rpopic2@gmail.com>
2022-11-18 22:25:57 +0900

    display session only if there are

--- scripts/git-changes.sh
+++ scripts/git-changes.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-RESULT=""
-if git status | grep -c push; then
+CWD=$1
+if test -z $CWD; then
+    CWD=$(pwd)
+fi
+
+RESULT=$(git remote get-url origin | cut -d/ -f5)
+RESULT=$RESULT/$(git -C $CWD branch --show-current)
+
+if git -C $CWD status | grep -c push; then
     RESULT="$RESULT↑"
 fi
-if git status | grep -c pull; then
+if git -C $CWD tatus | grep -c pull; then
     RESULT="$RESULT↓"
 fi
-if git status | grep -c 'tree clean'; then
+if git -C $CWD status | grep -c 'tree clean'; then
     echo $RESULT
     exit
+else
+    echo "$RESULT*"
 fi
-echo "$RESULT*"
